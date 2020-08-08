0cb3734b1b2926d13ab30db05578937431332ac0
Leandro <leandro.hurtado@utp.edu.co>
2020-08-08 17:36:04 -0500

    login creado

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Piedra Papel Tijeras</title>
+    </head>
+
+    <body>
+        <h1>Bienvenido a Piedra Papel Tijeras</h1>
+        <a href="login.php">Inicia sesion para jugar</a>
+    </body>
+</html>
--- login.php
+++ login.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <title>Login - Piedra PApel Tijeras</title>
     </head>
 
     <body>
+        <h1>Inicio de sesion</h1>
     </body>
 </html>
