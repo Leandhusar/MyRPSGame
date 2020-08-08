1b9ef87aa46e108001716c72777ab9477292e81b
Leandro <leandro.hurtado@utp.edu.co>
2020-08-08 17:45:47 -0500

    Espacios del login

--- login.php
+++ login.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Login - Piedra PApel Tijeras</title>
+        <title>Login - Piedra Papel Tijeras</title>
     </head>
 
     <body>
         <h1>Inicio de sesion</h1>
+        <p>Ingrese sus datos apropiadamente</p>
+        <form>
+            Nombre: <input type="text" name="who">
+            Clave:  <input type="password" name="pass">
+            <input type="submit" value="Iniciar sesion">
+        </form<
     </body>
 </html>
