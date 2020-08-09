454f8f878565e74986625369d723f8000ff8b528
Leandro <leandro.hurtado@utp.edu.co>
2020-08-08 21:10:47 -0500

    texto

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
     <body>
         <h1>Bienvenido a Piedra Papel Tijeras</h1>
-        <a href="login.php">Inicia sesion para jugar</br></a>
+        <a href="login.php">Please Log In</br></a>
         <a href="game.php">Ingresar aca implica un error<a>
     </body>
 </html>
